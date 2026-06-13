dac35bad3317c5e0e31516c5b69695a8c76ef064
Aaryan946 <mitraaaryan8@gmail.com>
2026-06-13 03:12:00 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+
+from groq import Groq
+import os
+
+client = Groq(api_key=("GROQ_API_KEY"))
+st.title("AI Chatbot")
+user_input=st.text_input("Ask something:")
+if st.button("Send"):
+    if user_input:
+        response=client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {"role":"user","content":user_input}
+            ]
+        )
+        st.write("Response:")
+        st.write(response.choices[0].message.content)
